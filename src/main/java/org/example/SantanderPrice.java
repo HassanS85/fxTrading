0dea0011082c4492b9e7a9fa8bde7729cb1c0946
HassanS85 <75862626+HassanS85@users.noreply.github.com>
2022-07-29 13:40:45 +0100

    Update SantanderPrice.java

--- src/main/java/org/example/SantanderPrice.java
+++ src/main/java/org/example/SantanderPrice.java
@@ -1,27 +1,71 @@
 package org.example;
 
 import com.opencsv.bean.CsvBindByPosition;
+import com.opencsv.bean.CsvDate;
 
 import java.util.Date;
+
 
 public class SantanderPrice {
 
         @CsvBindByPosition(position = 0)
-        private String uniqueId;
+        private String id;
 
         @CsvBindByPosition(position = 1)
-        private String instrumentName;
+        private String Name;
 
         @CsvBindByPosition(position = 2)
-        private Double currentBid;
+        private double bid;
 
         @CsvBindByPosition(position = 3)
-        private Double currentAsk;
+        private double ask;
 
         @CsvBindByPosition(position = 4)
+        @CsvDate("dd-MM-yyyy HH:mm:ss:SSS") // format of the date in the csv file
         private Date timestamp;
 
 
-        //  getters, setters, toString
-    }
+        //Getters and Setters
 
+        public String getId() {
+                return id;
+        }
+
+        public void setId(String id) {
+                this.id = id;
+        }
+
+        public String getName() {
+                return Name;
+        }
+
+        public void setName(String name) {
+                Name = name;
+        }
+
+        public double getBid() {
+                return bid;
+        }
+
+        public void setBid(double bid) {
+                this.bid = bid;
+        }
+
+        public double getAsk() {
+                return ask;
+        }
+
+        public void setAsk(double ask) {
+                this.ask = ask;
+        }
+
+        public Date getTimestamp() {
+                return timestamp;
+        }
+
+        public void setTimestamp(Date timestamp) {
+                this.timestamp = timestamp;
+        }
+
+
+}
